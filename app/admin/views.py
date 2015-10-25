75e51cf5bf4bed318b995b372abc27e635b4624d
liulixiang1988 <liulixiang1988@gmail.com>
2015-10-25 15:21:30 +0800

    admin edit the meal for the recommendations

--- app/admin/views.py
+++ app/admin/views.py
@@ -1,7 +1,9 @@
 # -*- coding:utf-8 -*-
 
-from flask import render_template
+from flask import render_template, request, redirect, url_for
+from .. import csrf
 from ..decorators import superuser_required
+from ..models import Meal
 from . import admin
 
 
@@ -10,6 +12,7 @@ def index():
     return 'hello admin!'
 
 
+@csrf.exempt
 @admin.route('/meals/<order_status>')
 @superuser_required
 def meals(order_status):
@@ -18,7 +21,13 @@ def meals(order_status):
     :param order_status: all, selected, unselected
     :return: the admin's meal list page
     """
-    return render_template('admin/meals.html')
+    if order_status in ('selected', 'unselected'):
+        selected = True if order_status=='selected' else False
+        meals = Meal.query.filter_by(is_selected=selected).order_by(Meal.id.desc())
+    else:
+        order_status = 'all'
+        meals = Meal.query.order_by(Meal.id.desc())
+    return render_template('admin/meals.html', meals=meals)
 
 
 @admin.route('/meal/<int:id>/edit')
@@ -29,6 +38,12 @@ def meal_edit(id):
     :param id: meal id
     :return: the admin's meal edit page
     """
-    return render_template('admin/meal_edit.html')
+    meal = Meal.query.get_or_404(id)
+    selected = request.args.get('selected')
+    if selected == 'yes':
+        meal.is_selected = True
+    elif selected == 'no':
+        meal.is_selected = False
+    return redirect(url_for('admin.meals', order_status='all'))
 
 
